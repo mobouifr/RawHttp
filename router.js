6bf55564aa986b0791ec538043a0de426caa5410
montassir <mountasser.bouifraden@gmail.com>
2026-05-11 18:35:39 +0100

    stage-2: implemented static file serving with fs module

--- router.js
+++ router.js
@@ -1,17 +1,82 @@
+const fs = require('fs');
 
 function router(parsedData, socket)
 {
-    if (parsedData.method === "GET" && parsedData.path === "/")
+    if (parsedData.method === "GET")
     {
-        const body = "Hello from my server";
-        const response =
-            "HTTP/1.1 200 OK\r\n" +
-            "Content-Type: text/plain\r\n" +
-            "\r\n" +
-            body;
+        if (parsedData.path === "/")
+        {
+            fs.readFile("./public/index.html", "utf-8", (error, data) => 
+            {
+                if (error)
+                {
+                    socket.write("HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Type: text/plain\r\n" +
+                            "\r\n" +
+                            "404 Not Found");
+                }
+                else
+                {
+                    socket.write( "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: text/html\r\n" +
+                            "\r\n" + data);
+                }
+                socket.end();
+            });
+        }
 
-        socket.write(response);
-        socket.end();
+        else if (parsedData.path === "/style.css")
+        {
+            fs.readFile("./public/style.css", "utf-8", (error, data) => 
+            {
+                if (error)
+                {
+                    socket.write("HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Type: text/plain\r\n" +
+                            "\r\n" +
+                            "404 Not Found");
+                }
+                else
+                {
+                    socket.write( "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: text/css\r\n" +
+                            "\r\n" + data);
+                }
+                socket.end();
+            });
+        }
+
+        else if (parsedData.path === "/app.js")
+        {
+            fs.readFile("./public/app.js", "utf-8", (error, data) => 
+            {
+                if (error)
+                {
+                    socket.write("HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Type: text/plain\r\n" +
+                            "\r\n" +
+                            "404 Not Found");
+                }
+                else
+                {
+                    socket.write( "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: application/javascript\r\n" +
+                            "\r\n" + data);
+                }
+                socket.end();
+            });
+        }
+        else
+        {
+            const response =
+                "HTTP/1.1 404 Not Found\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "\r\n" +
+                "404 Not Found";
+    
+            socket.write(response);
+            socket.end();
+        }
     }
 
     else if (parsedData.method === "POST" && parsedData.path === "/data")
@@ -22,7 +87,7 @@ function router(parsedData, socket)
 
         const response =
             "HTTP/1.1 200 OK\r\n" +
-            "Content-Type: text/plain\r\n" +
+            "Content-Type: application/json\r\n" +
             "\r\n" +
             body;
 
